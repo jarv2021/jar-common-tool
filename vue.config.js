35891a9cf3eb272c18552bd32a31045d6a67dd13
liangqiujie <708985420@qq.com>
2021-08-17 15:02:28 +0800

    Fix:review vueconfig

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,20 @@ const path = require("path");
 const CompressionWebpackPlugin = require("compression-webpack-plugin");
 
 module.exports = {
+  // //是否开启 productionSourceMap
   productionSourceMap: process.env.NODE_ENV !== "production",
-  lintOnSave: true, //是否开启eslint
+  // //是否开启 eslint
+  lintOnSave: true,
+  // 使用transpileDependencies属性配置node_modules中指定哪些文件夹或文件需要编译.
+  transpileDependencies: [/[/\\]node_modules[/\\]pdfjs-dist[/\\]/],
+  css: {
+    // 是否使用css分离插件 ExtractTextPlugin
+    extract: true,
+    // 开启 CSS source maps?
+    sourceMap: false,
+    // css预设器配置项
+    loaderOptions: {}
+  },
   // 自定义 webpack  配置
   chainWebpack: config => {
     // 自定义路径
@@ -20,10 +32,20 @@ module.exports = {
           priority: 10,
           chunks: "initial" // only package third parties that are initially dependent
         },
-        "pdf-dist": {
-          name: "pdf-dist",
+        "pdfjs-dist": {
+          name: "pdfjs-dist",
           priority: 20, // 优先级 20
-          test: /[\\/]node_modules[\\/]_?pdf-dist(.*)/
+          test: /[\\/]node_modules[\\/]_?pdfjs-dist(.*)/
+        },
+        vue: {
+          name: "vue",
+          priority: 30, // 优先级 20
+          test: /[\\/]node_modules[\\/]_?vue(.*)/
+        },
+        cropperjs: {
+          name: "cropperjs",
+          priority: 21, // 优先级 20
+          test: /[\\/]node_modules[\\/]_?cropperjs(.*)/
         }
       }
     });
@@ -54,7 +76,6 @@ module.exports = {
         raw: true
       });
   },
-  transpileDependencies: [/[/\\]node_modules[/\\]pdfjs-dist[/\\]/],
   configureWebpack: config => {
     // 生产环境下生效
     if (process.env.NODE_ENV === "production") {
